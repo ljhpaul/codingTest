8fec8647b86cf7c54490c4a337d6267d651cd953
이정헌 <colosair@gmail.com>
2026-01-26 17:58:30 +0900

    #1697 update

---
+++ src/baekjoon/roadmap/week01/boj14500/boj1697/Main.java
@@ -0,0 +1,44 @@
+package baekjoon.roadmap.week01.boj14500.boj1697;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	// static field
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+
+	// main
+	public static void main(String[] args) throws IOException {
+		// init
+		int answer = Integer.MAX_VALUE;
+		st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[][]
+
+		// get answer
+		// '-'
+		for(int i=0; i<)
+
+		// output
+		System.out.println(answer);
+	}
+
+}
+
+/*
+====================================
+# 문제 정리
+브루트 포스 (N^2 = 250000 < 2^7)
+실제 도형
+- '-', '|' : (1 * 4), (4 * 1)
+- 'ㅁ' : (2 * 2)
+- 'L' * 4, 'ㄱ' * 4 : (3 * 2), (2 * 3)
+- 's' * 2, 'ㄹ' * 2 : (3 * 2), (2 * 3)
+- 'ㅗ', 'ㅓ', 'ㅜ', 'ㅏ' : (3 * 2), (2 * 3)
+====================================
+# 슈도 코드
+각 도형 별로 저장
+====================================
+ */
---
+++ src/baekjoon/roadmap/week01/boj1697/Main.java
@@ -0,0 +1,71 @@
+package baekjoon.roadmap.week01.boj1697;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	// static field
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static final int MAX_SIZE = 100000;
+
+	static boolean[] visited = new boolean[MAX_SIZE + 1];
+	static int[] dist = new int[MAX_SIZE + 1];
+	static int n;
+	static int k;
+
+	// main
+	public static void main(String[] args) throws IOException {
+		// init
+		int answer = 0;
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+
+		// get answer
+		if(n >= k) {
+			System.out.println(n - k);
+			return;
+		}
+		BFS();
+		answer = dist[k];
+
+		// output
+		System.out.println(answer);
+	}
+
+	private static void BFS() {
+		ArrayDeque<Integer> q = new ArrayDeque<>();
+		q.add(n);
+		visited[n] = true;
+		dist[n] = 0;
+
+		while(!q.isEmpty()) {
+			int now = q.poll();
+
+			if(now == k) {
+				return;
+			}
+
+			int n1 = now - 1;
+			int n2 = now + 1;
+			int n3 = now * 2;
+
+			if(n1 >= 0 && !visited[n1]) {
+				q.add(n1);
+				visited[n1] = true;
+				dist[n1] = dist[now] + 1;
+			}
+			if(n2 <= MAX_SIZE && !visited[n2]) {
+				q.add(n2);
+				visited[n2] = true;
+				dist[n2] = dist[now] + 1;
+			}
+			if(n3 <= MAX_SIZE && !visited[n3]) {
+				q.add(n3);
+				visited[n3] = true;
+				dist[n3] = dist[now] + 1;
+			}
+		}
+	}
+}
