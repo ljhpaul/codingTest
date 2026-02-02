c80770a81adc1ee49aab557aecd3c10cdd04dc2e
이정헌 <colosair@gmail.com>
2026-02-02 11:01:19 +0900

    update

---
+++ src/swea/d2/p14510/Solution.java
@@ -0,0 +1,21 @@
+package swea.d2.p14510;
+
+class Solution {
+    // static field
+
+
+
+    // main
+    public static void main(String[] args) {
+
+    }
+}
+/*
+
+### 문제 분석
+- tc, N, N개의 나무 높이
+-
+
+### 슈도 코드
+
+ */
---
+++ src/swea/d4/p25697/Solution.java
@@ -0,0 +1,21 @@
+package swea.d4.p25697;
+
+class Solution {
+    // static field
+
+
+
+    // main
+    public static void main(String[] args) {
+
+    }
+}
+/*
+
+### 문제 분석
+- tc, N, N개의 나무 높이
+-
+
+### 슈도 코드
+
+ */
