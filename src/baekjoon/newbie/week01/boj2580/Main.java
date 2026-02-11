cdd779560bd1ab318faaaef647de5ee0fa050253
이정헌 <colosair@gmail.com>
2026-02-11 10:10:17 +0900

    update

--- src/baekjoon/newbie/week01/boj2580/Main.java
+++ src/baekjoon/newbie/week01/boj2580/Main.java
@@ -3,39 +3,39 @@ package baekjoon.newbie.week01.boj2580;
 import java.io.*;
 import java.util.*;
 
-public class Main {
+class Main {
     // static field
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
 
+    static int answer;
+    static int N;
     static int[][] grid;
-    static List<int[]> blanks;
-    static boolean[][] rowSelected;
-    static boolean[][] columnSelected;
-    static boolean[][] square3x3Selected;
+
+    static boolean[][] row;
+    static boolean[][] col;
+    static boolean[][] box;
 
     // main
     public static void main(String[] args) throws IOException {
         // init
+        answer = 0;
+        N = 0;
         grid = new int[9][9];
-        blanks = new ArrayList<>();
-        rowSelected = new boolean[9][9 + 1];        // 행 기준 : 1~9 까지 사용 여부
-        columnSelected = new boolean[9][9 + 1];     // 열 기준 : 1~9 까지 사용 여부
-        square3x3Selected = new boolean[9][9 + 1];  // 3x3 기준 : 1~9 까지 사용 여부
+        row = new boolean[9][9 + 1];
+        col = new boolean[9][9 + 1];
+        box = new boolean[9][9 + 1];
 
         // input
-        for (int i = 0; i < 9; i++) {
+        for(int i=0; i<9; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 9; j++) {
+            for(int j=0; j<9; j++) {
                 int num = Integer.parseInt(st.nextToken());
                 grid[i][j] = num;
-
                 if(num == 0) {
-                    blanks.add(new int[]{i, j});
+                    N++;
                 } else {
-                    rowSelected[i][num] = true;
-                    columnSelected[j][num] = true;
-                    square3x3Selected[getSquareIndex(i, j)][num] = true;
+                    setSelected(i, j, num, true);
                 }
             }
         }
@@ -43,107 +43,97 @@ public class Main {
         // solve
         dfs(0);
 
-        // output
-        output();
-    }
-
-    private static int getSquareIndex(int r, int c) {
-        int index = 0;
-        if(r < 3) {
-            if(c < 3) index = 0;
-            else if(c < 6) index = 1;
-            else if(c < 9) index = 2;
-        } else if(r < 6) {
-            if(c < 3) index = 3;
-            else if(c < 6) index = 4;
-            else if(c < 9) index = 5;
-        } else {
-            if(c < 3) index = 6;
-            else if(c < 6) index = 7;
-            else if(c < 9) index = 8;
-        }
-        return index;
-    }
-
-    private static void dfs(int idx) {
-        // base case
-        if(idx == blanks.size()) {
-            return;
-        }
-
-        // recursion case
-        for(int i = 1; i <= 9; i++) {
-            int r = blanks.get(idx)[0];
-            int c = blanks.get(idx)[1];
-            if(checkDuplication(r, c, i)) continue;  // 중복 검사
-            grid[r][c] = i;
-            dfs(idx + 1);
-            if(checkComplete(r, c, i)) break;
-        }
-    }
-
-    private static boolean checkDuplication(int r, int c, int num) {
-        // 행 중복 확인
-        if(rowSelected[r][num]) return true;
-
-        // 열 중복 확인
-        if(columnSelected[c][num]) return true;
-
-        // 3x3 중복 확인
-        if(square3x3Selected[getSquareIndex(r, c)][num]) return true;
-
-        // 사용되지 않은 수 확인됨
-        return false;
-    }
-
-    private static boolean checkComplete(int r, int c, int num) {
-        // 행 유일한 빈칸인지 확인
-        int rCnt = 0;
-        for(int i = 1; i <= 9; i++) {
-            if(i == num) continue;
-            if(rowSelected[r][i]) rCnt++;
-        }
-        if(rCnt == 8) return true;
-
-        // 열 유일한 빈칸인지 확인
-        int cCnt = 0;
-        for(int i = 1; i <= 9; i++) {
-            if(i == num) continue;
-            if(rowSelected[c][i]) cCnt++;
-        }
-        if(cCnt == 8) return true;
-
-        // 3x3 유일한 빈칸인지 확인
-        int sqCnt = 0;
-        for(int i = 1; i <= 9; i++) {
-            if(i == num) continue;
-            if(rowSelected[getSquareIndex(r, c)][i]) sqCnt++;
-        }
-        if(sqCnt == 8) return true;
-
-        // 사용되지 않은 수 확인됨
-        return false;
-    }
-
-    private static void output() {
-        for(int i = 0; i < 9; i++) {
-            for(int j = 0; j < 9; j++) {
+        // answer
+        for(int i=0; i<9; i++) {
+            for(int j=0; j<9; j++) {
                 System.out.print(grid[i][j] + " ");
             }
             System.out.println();
         }
+        br.close();
     }
 
+    // dfs
+    private static boolean dfs(int cnt) {
+        // base case
+        if(cnt == N) {
+            return true;
+        }
+
+        // find MRV
+        int[] mrv = findMRV();
+        int r = mrv[0];
+        int c = mrv[1];
+        List<Integer> nums = getNums(r, c);
+
+        // recursion case
+        for(int num : nums) {
+            grid[r][c] = num;
+            setSelected(r, c, num, true);
+
+            if(dfs(cnt + 1)) return true;
+
+            grid[r][c] = 0;
+            setSelected(r, c, num, false);
+        }
+        return false;
+    }
+
+    // set selected
+    private static void setSelected(int r, int c, int num, boolean b) {
+        row[r][num] = b;
+        col[c][num] = b;
+        box[getBoxIndex(r, c)][num] = b;
+    }
+
+    // get box index
+    private static int getBoxIndex(int r, int c) {
+        if(r < 3) {
+            if(c < 3)       return 0;
+            else if(c < 6)  return 1;
+            else            return 2;
+        } else if(r < 6) {
+            if(c < 3)       return 3;
+            else if(c < 6)  return 4;
+            else            return 5;
+        } else {
+            if(c < 3)       return 6;
+            else if(c < 6)  return 7;
+            else            return 8;
+        }
+    }
+
+    // find MRV
+    private static int[] findMRV() {
+        int min = Integer.MAX_VALUE;
+        int r = 0, c = 0;
+
+        for(int i=0; i<9; i++) {
+            for(int j=0; j<9; j++) {
+                if(grid[i][j] != 0) continue;
+                int cnt = 0;
+                for(int num=1; num<10; num++) {
+                    if(!row[i][num] && !col[j][num] && !box[getBoxIndex(i, j)][num]) cnt++;
+                }
+                if(cnt < min) {
+                    min = cnt;
+                    r = i;
+                    c = j;
+                    if(min == 1) return new int[]{r, c};
+                }
+            }
+        }
+
+        return new int[]{r, c};
+    }
+
+    // get able nums
+    private static List<Integer> getNums(int r, int c) {
+        List<Integer> nums = new ArrayList<>();
+        for(int num=1; num<10; num++) {
+            if(!row[r][num] && !col[c][num] && !box[getBoxIndex(r, c)][num]) nums.add(num);
+        }
+        return nums;
+    }
 
 }
-/*
-# 상태
-- 결정 :
-- 미정 :
-# 선택지
--
-# 제약
--
-# 종료 조건
--
- */
---
+++ src/swea/d3/p2805/Solution.java
@@ -0,0 +1,34 @@
+package swea.d3.p2805;
+
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    // static field
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int answer;
+
+    // main
+    public static void main(String[] args) throws IOException {
+        // test case
+        int T = Integer.parseInt(br.readLine());
+        for(int tc = 1; tc <= T; tc++) {
+            // init
+
+            // input
+
+            // solve
+
+            // answer
+            sb.append("#").append(tc).append(" ").append(answer).append("\n");
+        }
+
+        // output
+        System.out.println(sb);
+        br.close();
+    }
+
+}
