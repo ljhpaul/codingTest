2f9d9b4937912353106e1b0ac085e1528de6724f
colosair99 <colosair@gmail.com>
2026-02-17 14:55:30 +0900

    update

---
+++ src/baekjoon/ssafy/boj14502/Main2.java
@@ -0,0 +1,138 @@
+package baekjoon.ssafy.boj14502;
+
+import java.io.*;
+import java.util.*;
+
+public class Main2 {
+	// static field
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+
+	static int maxSafeArea, N, M;
+	static int[][] map;
+	static boolean[][] visited;
+	static List<int[]> virus, blank, wall;
+	
+	static int[] dr = {-1, 1, 0, 0}; // 상하좌우
+	static int[] dc = {0, 0, -1, 1};
+	
+	// main
+	public static void main(String[] args) throws IOException {
+		// init
+		maxSafeArea = 0;
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		map = new int[N][M];
+		virus = new ArrayList<>();
+		blank = new ArrayList<>();
+		
+		// input
+		for(int r=0; r<N; r++) {
+			st = new StringTokenizer(br.readLine());
+			for(int c=0; c<M; c++) {
+				map[r][c] = Integer.parseInt(st.nextToken());
+				if(map[r][c] == 2) {
+					virus.add(new int[] {r, c});
+				} else if(map[r][c] == 0) {
+					blank.add(new int[] {r, c});
+				}
+			}
+		}
+
+		// solve
+		setWall(0);
+
+		// output
+		System.out.println(maxSafeArea);
+		br.close();
+	}
+
+	// set 3 wall : combination NM C 3
+	private static void setWall(int cnt) {
+		// base
+		if(cnt == 3) {
+			int[][] tempMap = copyMap();
+			visited = new boolean[N][M];
+			spreadVirus(tempMap);
+//			debugMap(tempMap);
+			getSafeArea(tempMap);
+			return;
+		}
+		
+		// recursion
+		for(int[] pos : blank) {
+			int r = pos[0];
+			int c = pos[1];
+			if(map[r][c] == 0) {
+				map[r][c] = 1;	// set wall
+				setWall(cnt + 1);
+				map[r][c] = 0;	// restore
+			}
+		}
+	}
+	
+	// copy map
+	private static int[][] copyMap() {
+		int[][] copy = new int[N][M];
+		for(int r=0; r<N; r++) {
+			for(int c=0; c<M; c++) {
+				copy[r][c] = map[r][c];
+			}
+		}
+		return copy;
+	}
+
+	// spread virus : BFS
+	private static void spreadVirus(int[][] targetMap) {
+		Queue<int[]> q = new ArrayDeque<>();
+		for(int[] pos : virus) {
+			int r = pos[0];
+			int c = pos[1];
+			q.add(new int[] {r, c});
+			visited[r][c] = true;
+		}
+		
+		while(!q.isEmpty()) {
+			int[] cur = q.poll();
+			int r = cur[0];
+			int c = cur[1];
+			
+			for(int d=0; d<4; d++) {
+				int nr = r + dr[d];
+				int nc = c + dc[d];
+				if(nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
+				if(!visited[nr][nc] && targetMap[nr][nc] == 0) {
+					q.add(new int[] {nr, nc});
+					visited[nr][nc] = true;
+					targetMap[nr][nc] = 2; // set virus
+				}
+			}
+		}
+	}
+
+	// get safe area : BFS
+	private static void getSafeArea(int[][] targetMap) {
+		int safeArea = 0;
+		
+		for(int[] pos : blank) {
+			int r = pos[0];
+			int c = pos[1];
+			if(targetMap[r][c] == 0) {
+				safeArea++;
+			}
+		}
+		
+		maxSafeArea = Math.max(safeArea, maxSafeArea);
+	}
+	
+	// debug map
+	private static void debugMap(int[][] targetMap) {
+		for(int r=0; r<N; r++) {
+			for(int c=0; c<M; c++) {
+				System.out.print(targetMap[r][c] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
