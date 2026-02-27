0b643dbcd265ef662a7c7ddd3c63fcc833cad60d
이정헌 <colosair@gmail.com>
2026-02-27 11:14:18 +0900

    update

---
+++ src/swea/pro/p25007/UserSolution.java
@@ -0,0 +1,26 @@
+package swea.pro.p25007;
+
+import java.io.*;
+import java.util.*;
+
+class UserSolution {
+	
+	
+	
+	
+	
+	
+	
+    public void init(int N) {
+        return;
+    }
+
+    public void join(int mTime, int mID, int mX, int mY, int mLength) {
+        return;
+    }
+
+    public Solution.RESULT top5(int mTime) {
+        Solution.RESULT res = new Solution.RESULT();
+        return res;
+    }
+}
