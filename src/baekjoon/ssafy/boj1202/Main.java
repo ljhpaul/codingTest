9e9196a0a8a08446a5c566e762eef951a12e63ee
이정헌 <colosair@gmail.com>
2026-03-06 17:47:41 +0900

    update

---
+++ src/baekjoon/ssafy/boj1202/Main.java
@@ -0,0 +1,29 @@
+package baekjoon.ssafy.boj1202;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	// static field
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	
+	
+	// main
+	public static void main(String[] args) throws IOException {
+		// init
+		
+		
+		// input
+		
+
+		// solve
+		
+
+		// output
+		System.out.println(answer);
+		br.close();
+	}
+	
+	
+}
